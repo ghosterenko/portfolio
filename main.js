ed88af6ef3017bed379a77a2ca7136894a4c9c07
ghosterenko <arsenijkonkov243@gmail.com>
2026-02-17 15:27:35 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+export function MainHtml() {
+    let html = "<div>1</div>";
+    return html;
+}
+export function aboutHtml() {
+    let html = "<div>2</div>";
+    return html;
+}
+export function achivementHtml() {
+    let html = "<div>3</div>";
+    return html;
+}
+export function DocumentHtml() {
+    let html = "<div>4</div>";
+    return html;
+}
+export function WorksHtml() {
+    let html = "<div>5</div>";
+    return html;
+}
+export function CitizenHtml() {
+    let html = "<div>6</div>";
+    return html;
+}
